e7ba2c85a9c85534ff13f2c3a091379f13a64d36
Pukima <pukima@pukima.site>
2023-12-27 19:16:08 +0100

    Update run.sh to use 'exec' for npm run start

--- fitbit-2-mqtt/run.sh
+++ fitbit-2-mqtt/run.sh
@@ -5,4 +5,4 @@ set +u
 MQTT_HOST=$(bashio::services mqtt "host") \
 MQTT_USER=$(bashio::services mqtt "username") \
 MQTT_PASSWORD=$(bashio::services mqtt "password") \
-npm run start
+exec npm run start
