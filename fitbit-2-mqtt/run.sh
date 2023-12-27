e9a44429461c57e38d4f717fd70a639067e54a44
Pukima <pukima@pukima.site>
2023-12-27 19:47:41 +0100

    another fix?

--- fitbit-2-mqtt/run.sh
+++ fitbit-2-mqtt/run.sh
@@ -5,4 +5,4 @@ set +u
 MQTT_HOST=$(bashio::services mqtt "host") \
 MQTT_USER=$(bashio::services mqtt "username") \
 MQTT_PASSWORD=$(bashio::services mqtt "password") \
-exec npm run start
+npm run start
